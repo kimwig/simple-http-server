5dc283c2cda860a97b8f4aad8b5224a7f73edd2d
Kim <54209113+kimwig@users.noreply.github.com>
2024-10-10 22:06:38 +0200

    Refactored init server and handle connection functions to improve modularity and maintainability

--- src/main.c
+++ src/main.c
@@ -1,9 +1,87 @@
 #include "main.h"
 
-void handle_request(int *peer_fd) {
-    char buffer[BUFFER_SIZE] = {0};
+int initialize_server(server_context_t *server_ctx, int port) {
 
-    read(*peer_fd, &buffer, BUFFER_SIZE);
+    // Initialize the server_addr struct
+    server_ctx->server_addr.sin_family = DOMAIN_IPV4;
+    server_ctx->server_addr.sin_addr.s_addr = LOOPBACK_ADDRESS;
+    server_ctx->server_addr.sin_port = htons(port);
+
+    server_ctx->server_addr_len = sizeof(server_ctx->server_addr);
+
+    // Create the socket
+    server_ctx->server_fd = socket(DOMAIN_IPV4, PROTOCOL_TCP, PROTOCOL_NUMBER);
+    if (server_ctx->server_fd < 0)
+        return ERR_SOCKET;
+
+    // Bind IP address and port to socket
+    if (bind(server_ctx->server_fd, (struct sockaddr *)&server_ctx->server_addr, server_ctx->server_addr_len) < 0) {
+        close(server_ctx->server_fd);
+        return ERR_BIND;
+    }
+
+    // Start listening for incoming connections
+    if (listen(server_ctx->server_fd, MAX_CONNECTION_BACKLOG) < 0) {
+        close(server_ctx->server_fd);
+        return ERR_LISTEN;
+    }
+
+    printf("Listening for incoming connections on: %s, port: %d\n",
+            inet_ntoa(server_ctx->server_addr.sin_addr), // Converts the ip address from network byte order to host byte order (not thread-safe)
+            ntohs(server_ctx->server_addr.sin_port)); // Converts the port from network byte order to host byte order
+    return SUCCESS;
+}
+
+int handle_peer_connections(server_context_t *server_ctx, peer_context_t *peer_ctx) {
+    // Accept incoming connection
+    if (peer_ctx->peer_fd = accept(server_ctx->server_fd,
+    (struct sockaddr *)&server_ctx->server_addr,
+    &server_ctx->server_addr_len) < 0) {
+        close(peer_ctx->peer_fd);
+        return ERR_ACCEPT;
+    }
+    return SUCCESS;
+}
+
+int handle_request(peer_context_t *peer_ctx) {
+    ssize_t bytes_read;
+    size_t total_bytes_read = 0;
+
+    peer_ctx->request_buffer = (char *)malloc(INITIAL_BUFFER_SIZE);
+    if (peer_ctx->request_buffer == NULL) {
+        close(peer_ctx->peer_fd);
+        return ERR_MALLOC;
+    }
+    peer_ctx->request_size = INITIAL_BUFFER_SIZE;
+
+    while ((bytes_read = read(peer_ctx->peer_fd, peer_ctx->request_buffer + total_bytes_read,
+    peer_ctx->request_size - total_bytes_read)) > 0) {
+        total_bytes_read += bytes_read;
+
+        if (total_bytes_read == peer_ctx->request_size) {
+            size_t new_size = peer_ctx->request_size * 2;
+            char *new_buffer = (char *)realloc(peer_ctx->request_buffer, new_size);
+            if (new_buffer == NULL) {
+                free(peer_ctx->request_buffer);
+                close(peer_ctx->peer_fd);
+                return ERR_REALLOC;
+            }
+            peer_ctx->request_buffer = new_buffer;
+            peer_ctx->request_size = new_size;
+        }
+    }
+
+    if (bytes_read < 0) {
+        close(peer_ctx->peer_fd);
+        return ERR_READ;
+    }
+
+    printf("Received request:\n%s\n", peer_ctx->request_buffer);
+
+    //Call handle response
+
+    /*
+    read(pper_ctx->peer_fd, &buffer, BUFFER_SIZE);
         printf("%.*s", BUFFER_SIZE, buffer);
         char method[10], path[100];
         sscanf(buffer, "%s %s", method, path);
@@ -18,78 +96,28 @@ void handle_request(int *peer_fd) {
             FILE *file = fopen(full_path, "r");
             if (file != NULL) {
                 char response[BUFFER_SIZE] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-                send(*peer_fd, response, strlen(response), 0);
+                send(peer_ctx->peer_fd, response, strlen(response), 0);
                 printf("%.*s", BUFFER_SIZE, response);
 
                 char file_buffer[BUFFER_SIZE];
                 while (fgets(file_buffer, BUFFER_SIZE, file)) {
-                    send(*peer_fd, file_buffer, strlen(file_buffer), 0);
+                    send(pper_ctx->peer_fd, file_buffer, strlen(file_buffer), 0);
                 }
                 fclose(file);
             } else {
                 char *not_found = "HTTP/1.1 404 Not Found\r\n\r\n";
-                send(*peer_fd, not_found, strlen(not_found), 0);
+                send(peer_ctx->peer_fd, not_found, strlen(not_found), 0);
                 printf("%.*s", strlen(not_found), not_found);
             }
-        }
+        }*/
 
-        close(*peer_fd);
+    //close(peer_ctx->peer_fd);
+    return SUCCESS;
 }
 
-void handle_response(int *peer_fd) {
+int handle_response(peer_context_t *peer_ctx) {
+    return SUCCESS;
 
-}
-
-void initialize_server(int *server_fd, struct sockaddr_in *server_addr, socklen_t *server_addr_size) {
-
-    // Initialize the server_addr struct
-    server_addr->sin_family = DOMAIN_IPV4;
-    server_addr->sin_addr.s_addr = LOOPBACK_ADDRESS;
-    server_addr->sin_port = PORT;
-
-    *server_addr_size = sizeof(*server_addr);
-
-    // Create the socket
-    *server_fd = socket(DOMAIN_IPV4, PROTOCOL_TCP, PROTOCOL_NUMBER);
-    if (server_fd < 0)
-        handle_error("Server socket creation failed");
-
-    // Bind IP address and port to socket
-    if (bind(*server_fd, (struct sockaddr *)&server_addr, *server_addr_size) < 0) {
-        close(*server_fd);
-        handle_error("Server socket bind failed");
-    }
-
-    // Start listening for incoming connections
-    if (listen(*server_fd, MAX_CONNECTION_BACKLOG) < 0) {
-        close(*server_fd);
-        handle_error("Server socket listening failed");
-    }
-
-    printf("Listening for incoming connections on: %s, port: %d\n",
-            inet_ntoa(server_addr->sin_addr), // Converts the ip address from network byte order to host byte order (not thread-safe)
-            ntohs(server_addr->sin_port)); // Converts the port from network byte order to host byte order
-}
-
-void handle_peer_connections(int *peer_fd) {
-    char buffer[BUFFER_SIZE] = {0};
-
-    int server_fd;
-    struct sockaddr_in server_addr;
-    socklen_t server_addr_size;
-    initialize_server(&server_fd, &server_addr, &server_addr_size);
-
-    // Accept incoming connection
-    *peer_fd = accept(server_fd,
-    (struct sockaddr *)&server_addr,
-    &server_addr_size);
-
-    if (peer_fd < 0) {
-        close(*peer_fd);
-        handle_error("Peer connection failed");
-    }
-
-    handle_request(peer_fd);
 }
 
 int main() {
--- src/main.h
+++ src/main.h
@@ -14,14 +14,43 @@
 #define PROTOCOL_NUMBER 0
 #define MAX_CONNECTION_BACKLOG 50
 #define LOOPBACK_ADDRESS inet_addr("127.0.0.1");
-#define PORT htons(8080);
+#define PORT 8080;
 #define BUFFER_SIZE 1024
+#define INITIAL_BUFFER_SIZE 1024
+#define MAX_BUFFER_SIZE 1024 * 1024
 
 #define handle_error(message) do { perror(message); exit(EXIT_FAILURE); } while(0)
 
-void initialize_server();
-void handle_peer_connections();
-void handle_request();
-void handle_response();
+enum {
+    SUCCESS = 0,
+    ERR_SOCKET = 1,
+    ERR_BIND = 2,
+    ERR_LISTEN = 3,
+    ERR_ACCEPT = 4,
+    ERR_MALLOC = 5,
+    ERR_REALLOC = 6,
+    ERR_READ = 7,
+};
+
+typedef struct {
+    int server_fd;
+    struct sockaddr_in server_addr;
+    socklen_t server_addr_len;
+} server_context_t;
+
+typedef struct {
+    int peer_fd;
+    struct sockaddr_in peer_addr;
+    int peer_addr_len;
+    char *request_buffer;
+    size_t request_size;
+    char * response_buffer;
+    size_t response_size;
+} peer_context_t;
+
+int initialize_server();
+int handle_peer_connections();
+int handle_request();
+int handle_response();
 
 #endif

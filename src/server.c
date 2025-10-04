cb90251b38eea00bb58e96d4012f60ff404a67fd
kimwig <54209113+kimwig@users.noreply.github.com>
2025-10-04 21:15:47 +0200

    Merged server_ctx struct with http_server struct

--- src/server.c
+++ src/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "server.h"
@@ -13,43 +14,21 @@ struct http_server_s* http_server_init(const int port) {
         return NULL;
     }
 
-    struct server_context_s *p_server_ctx = &http_server->server_ctx;
-    struct sockaddr_in *p_server_addr = &p_server_ctx->server_addr;
+    struct sockaddr_in *p_server_addr = &http_server->server_addr;
 
+    memset(&p_server_addr, 0, sizeof(p_server_addr));
     p_server_addr->sin_family = AF_IPV4;
     p_server_addr->sin_addr.s_addr = inet_addr(DEFAULT_IP_ADDR);
     p_server_addr->sin_port = htons(port);
-    p_server_ctx->server_addr_len = sizeof(p_server_ctx->server_addr);
+    http_server->server_addr_len = sizeof(p_server_addr);
 
     return http_server;
 }
 
-int http_server_listen(struct http_server_s *p_http_server) {
+int create_socket(struct http_server_s *p_http_server) {
 
-    struct server_context_s *p_server_ctx = &p_http_server->server_ctx;
-
-    int create_socket_status = create_socket(p_server_ctx);
-    int bind_socket_status = bind_socket(p_server_ctx);
-    int listen_socket_status = listen_socket(p_server_ctx);
-
-    if (create_socket_status != 0 ||
-        bind_socket_status != 0 ||
-        listen_socket_status != 0) {
-            cleanup_server(p_http_server);
-            return 1;
-        }
-
-    printf("Listening for incoming connections on: %s, port: %d\n",
-            inet_ntoa(p_server_ctx->server_addr.sin_addr),
-            ntohs(p_server_ctx->server_addr.sin_port));
-
-    return 0;
-}
-
-int create_socket(struct server_context_s *p_server_ctx) {
-
-    p_server_ctx->server_fd = socket(AF_IPV4, PROTOCOL_TCP, PROTOCOL_NUMBER_TCP);
-    if (p_server_ctx->server_fd == -1) {
+    p_http_server->server_fd = socket(AF_IPV4, PROTOCOL_TCP, PROTOCOL_NUMBER_TCP);
+    if (p_http_server->server_fd == -1) {
         perror("Error creating socket");
         return 1;
     }
@@ -57,9 +36,9 @@ int create_socket(struct server_context_s *p_server_ctx) {
     return 0;
 }
 
-int bind_socket(struct server_context_s *p_server_ctx) {
+int bind_socket(struct http_server_s *p_http_server) {
 
-    int bind_status = bind(p_server_ctx->server_fd, (struct sockaddr *)&p_server_ctx->server_addr, p_server_ctx->server_addr_len);
+    int bind_status = bind(p_http_server->server_fd, (struct sockaddr *)&p_http_server->server_addr, p_http_server->server_addr_len);
 
     if (bind_status == -1) {
         perror("Error binding socket");
@@ -69,9 +48,9 @@ int bind_socket(struct server_context_s *p_server_ctx) {
     return 0;
 }
 
-int listen_socket(struct server_context_s *p_server_ctx) {
+int listen_socket(struct http_server_s *p_http_server) {
 
-    int listen_status = listen(p_server_ctx->server_fd, MAX_CONNECTION_BACKLOG);
+    int listen_status = listen(p_http_server->server_fd, MAX_CONNECTION_BACKLOG);
 
     if (listen_status == -1) {
         perror("Error listening on socket");
@@ -81,12 +60,30 @@ int listen_socket(struct server_context_s *p_server_ctx) {
     return 0;
 }
 
+int http_server_listen(struct http_server_s *p_http_server) {
+
+    int create_socket_status = create_socket(p_http_server);
+    int bind_socket_status = bind_socket(p_http_server);
+    int listen_socket_status = listen_socket(p_http_server);
+
+    if (create_socket_status != 0 ||
+        bind_socket_status != 0 ||
+        listen_socket_status != 0) {
+            cleanup_server(p_http_server);
+            return 1;
+        }
+
+    printf("Listening for incoming connections on: %s, port: %d\n",
+            inet_ntoa(p_http_server->server_addr.sin_addr),
+            ntohs(p_http_server->server_addr.sin_port));
+
+    return 0;
+}
+
 void cleanup_server(struct http_server_s *p_http_server) {
 
-    const struct server_context_s *p_server_ctx = &p_http_server->server_ctx;
-
-    if (p_server_ctx->server_fd >= 0) {
-        close(p_server_ctx->server_fd);
+    if (p_http_server->server_fd >= 0) {
+        close(p_http_server->server_fd);
     }
 
     free(p_http_server);
--- src/server.h
+++ src/server.h
@@ -9,21 +9,17 @@
 #define DEFAULT_IP_ADDR "127.0.0.1"
 #define MAX_CONNECTION_BACKLOG 50
 
-struct server_context_s {
+struct http_server_s {
     int server_fd;
     struct sockaddr_in server_addr;
     socklen_t server_addr_len;
 };
 
-struct http_server_s {
-    struct server_context_s server_ctx;
-};
-
 struct http_server_s* http_server_init(const int port);
+int create_socket(struct http_server_s *p_http_server);
+int bind_socket(struct http_server_s *p_http_server);
+int listen_socket(struct http_server_s *p_http_server);
 int http_server_listen(struct http_server_s *p_http_server);
-int create_socket(struct server_context_s *p_server_ctx);
-int bind_socket(struct server_context_s *p_server_ctx);
-int listen_socket(struct server_context_s *p_server_ctx);
-void cleanup_server(struct http_server_s *p_server_ctx);
+void cleanup_server(struct http_server_s *p_http_server);
 
 #endif
